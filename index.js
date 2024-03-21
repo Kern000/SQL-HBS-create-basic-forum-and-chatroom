8e307692b0d7e2f1f9582f14bc7de574033a956e
unknown <holupkern@gmail.com>
2024-03-21 21:27:20 +0800

    completed basic show threads

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
+const { createConnection } = require("mysql2/promise")
 require("dotenv").config();
 
 let app = express();
@@ -11,7 +12,36 @@ app.use(express.urlencoded({extended:false}));
 wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
+let connection;
+
+async function main(){
+
+    connection = await createConnection({
+        'host':process.env.DB_HOST,
+        'user':process.env.DB_USER,
+        'database':process.env.DB_NAME,
+        'password':process.env.DB_PASSWORD
+    })
+
+
+    app.get("/threads", async function (req, res) {
+        let [threads] = await connection.execute("SELECT Threads.*, Users.userName FROM Threads JOIN USERS ON Threads.user_id = USERS.userId ORDER BY Threads.threadId");
+        res.render("index", {
+            threadsData: threads
+        });
+    })
+}
+
+
+
 app.get("/", (req,res)=> res.send("Hello, World"));
+
 app.listen(3000, ()=>{
     console.log("Server is running")
 })
+
+main();
+
+
+
+
